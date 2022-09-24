4b4ca45d1c260a06346cf26afdb036a5b316b200
Jakub Abramczyk <jakub.abramczyk@euvic.pl>
2022-09-24 18:50:01 +0200

    navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<nav class="space-between">
+			<router-link to="/"> Home </router-link>
+			<div>
+				<router-link to="/register"> Register </router-link> |
+				<router-link to="/sign-in"> Login </router-link>
+			</div>
+		</nav>
+		<router-view />
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+
+})
+</script>
